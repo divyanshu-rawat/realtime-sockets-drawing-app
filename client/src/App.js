3b6fb9ac7480645503a87ea7d979b14dfd6e643b
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-10-23 16:09:53 +0530

    project started

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Drawing App</h2>
+        </div>
+        Welcome!
+      </div>
+    );
+  }
+}
+
+export default App;
